57f339d3c360c725711df2ff89153378ddf43250
GitHub Action <action@github.com>
2022-10-28 01:46:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/lazyyuxin/gradle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lazyyuxin/gradle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lazyyuxin.gradle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lazyyuxin.gradle.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lazyyuxin/gradle/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lazyyuxin/gradle/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lazyyuxin.gradle.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lazyyuxin.gradle.MyBundle
 
 class MyProjectService(project: Project) {
 
